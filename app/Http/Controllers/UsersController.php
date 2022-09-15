e73a606acb7cf69575b733d9d1550177f559be1b
mrou-github <1593692219@qq.com>
2022-09-15 17:29:33 +0800

    更改用户资料

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -8,14 +8,33 @@ use Illuminate\Support\Facades\Auth;
 
 class UsersController extends Controller
 {
-    public function create()
-    {
-        return view('users.create');
-    }
+
 
     public function show(User $user)
     {
         return view('users.show', compact('user'));
+    }
+
+    public function edit(User $user)
+    {
+        return view('users.edit', compact('user'));
+    }
+
+    public function update(User $user, Request $request)
+    {
+        $this->validate($request, [
+            'name' => 'required|max:50',
+            'password' => 'required|confirmed|min:6'
+        ]);
+
+        $user->update([
+            'name' => $request->name,
+            'password' => bcrypt($request->password),
+        ]);
+
+        session()->flash('success', '个人资料更新成功！');
+        
+        return redirect()->route('users.show', $user->id);
     }
 
     public function store(Request $request)
