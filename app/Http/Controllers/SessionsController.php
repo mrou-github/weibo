fcaae3ae7a112d686855110a0f9efe49adc0f0c4
mrou-github <1593692219@qq.com>
2022-09-15 17:13:40 +0800

    登录成功后导航逻辑

--- app/Http/Controllers/SessionsController.php
+++ app/Http/Controllers/SessionsController.php
@@ -11,6 +11,7 @@ class SessionsController extends Controller
         return view('sessions.create');
     }
 
+
     public function store(Request $request)
     {
         $credentials = $this->validate($request, [
