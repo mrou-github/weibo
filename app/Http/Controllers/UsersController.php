73adfa815d9ef88e0b0e1d69a2196a804cc9debf
mrou-github <1593692219@qq.com>
2022-09-16 10:13:57 +0800

    用户微博列表

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -30,6 +30,12 @@ class UsersController extends Controller
 
     public function show(User $user)
     {
+
+        $statuses = $user->statuses()
+            ->orderBy('created_at', 'desc')
+            ->paginate(10);
+        return view('users.show', compact('user', 'statuses'));
+
         return view('users.show', compact('user'));
     }
 
